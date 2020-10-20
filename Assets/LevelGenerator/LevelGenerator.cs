9d33d4d59504338bc1c2963032f41e62da33e284
Salvatore Laiso <salvatorelaiso@outlook.it>
2020-10-20 15:16:25 +0200

    feat: create clones of template scene

---
+++ Assets/LevelGenerator/LevelGenerator.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LevelGenerator
+{
+    internal static class LevelGenerator
+    {
+        private const string TemplateLevelPath = "Assets/LevelGenerator/LevelTemplate/LevelTemplate.unity";
+        private const string TemplateLevelName = "LevelTemplate";
+        
+        private const string LevelsPath = "Assets/Scenes/";
+        private const string LevelString = "Level";
+        private const string LevelExtension = ".unity";
+
+        public static void CloneLevel(int levelIndex)
+        {
+            var suffix = $"{levelIndex:00}";
+            var levelName = LevelString + suffix + LevelExtension;
+            var dest = LevelsPath + levelName;
+            var scene = EditorSceneManager.OpenScene(TemplateLevelPath, OpenSceneMode.Single);
+            if (EditorSceneManager.SaveScene(scene, dest, true))
+            {
+                EditorSceneManager.OpenScene(dest, OpenSceneMode.Single);
+            }
+            else
+            {
+                Debug.Log("Could not create the scene!");
+            }
+        }
+    }
+}
+#endif
---
+++ Assets/LevelGenerator/LevelGeneratorWindow.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace LevelGenerator
+{
+    public class LevelGeneratorWindow : EditorWindow
+    {
+        private const string LevelsPath = "Assets/Scenes";
+        private const int LevelNumberCharacters = 2;
+        
+        private int levelNumber = 0;
+        
+        private int rows;
+        private const int MinRows =   4;
+        private const int MaxRows = 100;
+        private int columns;
+        private const int MinColumns =   4;
+        private const int MaxColumns = 100;
+
+        // Add menu named "Generator" to the Window menu
+        [MenuItem("Window/Generator")]
+        private static void Init()
+        {
+            // Get existing open window or if none, make a new one:
+            var window = (LevelGeneratorWindow) GetWindow(typeof(LevelGeneratorWindow));
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label($"Level {levelNumber:00}", EditorStyles.boldLabel);
+            rows = EditorGUILayout.IntSlider("Rows", rows, MinRows, MaxRows);
+            columns = EditorGUILayout.IntSlider("Columns", columns, MinColumns, MaxColumns);
+            if (GUILayout.Button("Generate"))
+            {
+                LevelGenerator.CloneLevel(levelNumber);
+            }
+        }
+
+        private void OnInspectorUpdate()
+        {
+            levelNumber = NextLevelIndex();
+        }
+
+        private static int NextLevelIndex()
+        {
+            // Check the highest Level already created
+            var lastLevel =
+                Directory.EnumerateFiles(LevelsPath, "Level??.unity").Last();
+            var extensionDotIndex =
+                lastLevel.LastIndexOf('.');
+            var number =
+                int.Parse(lastLevel.Substring(extensionDotIndex - LevelNumberCharacters, LevelNumberCharacters));
+            // Return the next int
+            return ++number;
+        }
+    }
+}
