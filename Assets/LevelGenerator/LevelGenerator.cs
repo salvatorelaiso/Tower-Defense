e6ad225c948b381fe9bfbd0a968bbdfd91f15287
Salvatore Laiso <salvatorelaiso@outlook.it>
2020-10-21 20:21:14 +0200

    feat: instantiate start and end points

--- Assets/LevelGenerator/LevelGenerator.cs
+++ Assets/LevelGenerator/LevelGenerator.cs
@@ -35,23 +35,50 @@ namespace LevelGenerator
 
         private static void Init(Scene newScene, int rows, int columns)
         {
+            var start = Resources.Load("LevelGenerator/Prefabs/START");
+            var end = Resources.Load("LevelGenerator/Prefabs/END");
             var node = Resources.Load("LevelGenerator/Prefabs/Node");
             const float nodeDim = 5f;
 
             var nodes = GameObject.Find("Nodes");
+            
             for (int i = 0; i < rows; i++)
             {
                 var row = new GameObject($"Row ({i})");
                 row.transform.parent = nodes.transform;
                 for (int j = 0; j < columns; j++)
                 {
-                    var position = new Vector3(j * nodeDim, 0, -i * nodeDim);
-                    Object.Instantiate(node,
-                            parent: row.transform,
+                    // START position
+                    if (i == j && j == 1)
+                    {
+                        var position = new Vector3(j * nodeDim, 2.5f, -i * nodeDim);
+                        var obj = Object.Instantiate(start,
                             position: position,
-                            rotation: Quaternion.identity)
-                        .name = $"Node ({j})";
-
+                            rotation: Quaternion.identity) as GameObject;
+                        obj.name = "START";
+                        obj.transform.SetSiblingIndex(nodes.transform.GetSiblingIndex()+1);
+                    }
+                    
+                    // END position
+                    else if (i == rows-2 && j == columns-2)
+                    {
+                        var position = new Vector3(j * nodeDim, 2.5f, -i * nodeDim);
+                        var obj = Object.Instantiate(end,
+                            position: position,
+                            rotation: Quaternion.identity) as GameObject;
+                        obj.name = "END";
+                        obj.transform.SetSiblingIndex(nodes.transform.GetSiblingIndex()+2);
+                    }
+                    
+                    else
+                    {
+                        var position = new Vector3(j * nodeDim, 0, -i * nodeDim);
+                        Object.Instantiate(node,
+                                parent: row.transform,
+                                position: position,
+                                rotation: Quaternion.identity)
+                            .name = $"Node ({j})";
+                    }
                 }
             }
         }
