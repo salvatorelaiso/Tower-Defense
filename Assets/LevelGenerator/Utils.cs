22414dda7d201211bb2ddec5734756cbfbc63566
Salvatore Laiso <salvatorelaiso@outlook.it>
2020-10-25 22:04:04 +0100

    Fixed: Random
    Creating new Random every time Shuffle was called caused randomness loss on sequential calls

--- Assets/LevelGenerator/Utils.cs
+++ Assets/LevelGenerator/Utils.cs
@@ -5,13 +5,14 @@ namespace LevelGenerator
 {
     internal static class Utils
     {
+        private static readonly Random Random = new Random();
+        
         internal static void Shuffle<T>(this IList<T> list)  
-        {  
-            var rng = new Random();
+        {
             var n = list.Count;  
             while (n > 1) {  
                 n--;  
-                var k = rng.Next(n + 1);  
+                var k = Random.Next(n + 1);  
                 var value = list[k];  
                 list[k] = list[n];  
                 list[n] = value;  
